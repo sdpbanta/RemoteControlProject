c37c8535a745d4f9e291de0c7f6314570c446980
sdpbanta <paul.banta@skilldistillery.com>
2019-11-08 16:44:37 -0700

    Cleaned Up Comments - main

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -2,15 +2,10 @@
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
 
 	}
-	
+
 	// TODO: Add pressButton() method
 	// TODO: Add turnOn() method
-	
 
 }
